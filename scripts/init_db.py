4f8a08d14dce4e61f71062534e27b6ae822e67f5
Dominik Seitz <ds243@hdm-stuttgart.de>
2026-01-08 19:44:06 +0100

    import models for init-command

--- scripts/init_db.py
+++ scripts/init_db.py
@@ -3,6 +3,9 @@ from app.db import Base, engine
 
 
 def init_db():
+    # MUST import models to register them with Base to create tables
+    from app.models import Account, Notification, Cryptocurrency  # noqa: F401
+
     Base.metadata.create_all(bind=engine)
     print("✓ Database tables created")
 
