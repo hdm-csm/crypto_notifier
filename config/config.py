798a619595751629054cf9017f64b47afcf94611
Dominik Seitz <ds243@hdm-stuttgart.de>
2026-01-15 18:15:04 +0100

    delete comments

--- config/config.py
+++ config/config.py
@@ -1,11 +1,7 @@
 import os
-
 from dotenv import load_dotenv
 
-# Load .env file depending on environment
-# defaults to DEV if not set, needs to be set like : "export ENV=PRD"
 ENV = os.getenv("ENV", "DEV")
-# print("ENV =", ENV)
 
 if ENV == "DEV":
     load_dotenv(".env.dev")
@@ -18,13 +14,11 @@ else:
 class Config:
     """Centralized configuration"""
 
-    # USED ALREADY!!!
     DB_HOST = os.getenv("DB_HOST")
     DB_PORT = os.getenv("DB_PORT")
     MYSQL_DATABASE = os.getenv("MYSQL_DATABASE")
     MYSQL_USER = os.getenv("MYSQL_USER")
     MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD")
-    # mysqlconnector
     DATABASE_URL = os.getenv(
         "DATABASE_URL",
         (f"mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}@" f"{DB_HOST}:{DB_PORT}/{MYSQL_DATABASE}"),
