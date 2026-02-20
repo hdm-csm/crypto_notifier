6a3c1a0eb82f004e1cc2ba53b382d5761db9f6b0
Dominik Seitz <ds243@hdm-stuttgart.de>
2026-02-20 15:08:26 +0100

    move import

--- app/bots/discord/custom_bot.py
+++ app/bots/discord/custom_bot.py
@@ -1,13 +1,13 @@
 from app.bots.discord.custom_context import CustomContext
 from app.services.account_lookup_service import AccountLookupService
 from discord.ext import commands
+from app.bots.discord.custom_tree import CustomTree
 
 
 class CustomDiscordBot(commands.Bot):
     """Custom bot that uses CustomContext for all commands."""
 
     def __init__(self, account_lookup_service: AccountLookupService, **kwargs):
-        from app.bots.discord.custom_tree import CustomTree
 
         super().__init__(tree_cls=lambda bot: CustomTree(bot, account_lookup_service), **kwargs)  # type: ignore[arg-type]
         self.account_lookup_service = account_lookup_service
